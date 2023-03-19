e4587b8605df84a63a9348d325dea89e15bc058c
Rajesh Pillai <pillai.rajesh@gmail.com>
2023-03-19 11:28:53 +0530

    Todos and done with formatting

--- src/main.rs
+++ src/main.rs
@@ -5,41 +5,107 @@ use ncurses::*;
 const REGULAR_PAIR: i16 = 0;
 const HIGHLIGHT_PAIR: i16 = 1;
 
+type Id = usize;
+
+
+#[derive(Default)]
+struct Ui{
+    list_curr: Option<Id>,
+    row: usize,
+    col: usize,
+}
+
+impl Ui {
+    fn begin(&mut self, row: usize, col: usize) {
+        self.row = row;
+        self.col = col;
+    }
+    fn begin_list(&mut self, id: Id) {
+        assert!(self.list_curr.is_none(), "Nested lists are not allowed!");
+        self.list_curr = Some(id);
+    }
+
+   
+
+    fn list_element(&mut self, label: &str, id: Id) {
+        let id_curr = self.list_curr.expect("Not allowed to create list elements outside of a list!");
+        self.label( label,  {
+            if id_curr == id {
+                HIGHLIGHT_PAIR
+            } else {
+                REGULAR_PAIR
+            }
+        });
+    }
+
+    fn label(&mut self, text: &str, pair: i16) {
+        mv(self.row as i32, self.col as i32);
+        attron(COLOR_PAIR(pair));
+        addstr(text);
+        attroff(COLOR_PAIR(pair));
+        self.row += 1;
+    }
+
+    fn end_list(&mut self) {
+        self.list_curr = None;
+    }
+
+    fn end(&mut self) {
+    }
+}
+
 fn main() {
     initscr();
     noecho();
     curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+    
     start_color();
     init_pair(REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK)    ;
     init_pair(HIGHLIGHT_PAIR, COLOR_BLACK, COLOR_WHITE);
     
     let mut quit = false;
 
-    let todos = vec![
-        "Learn Rust", 
-        "Learn Zig",
-        "Learn Kubernetes"
+    let todos: Vec<String> = vec![
+        "Learn Rust".to_string(), 
+        "Learn Zig".to_string(),
+        "Learn Kubernetes".to_string()
     ];
 
     let mut todo_curr: usize = 0;
+    let dones = vec![
+        "Learn C".to_string(), 
+        "Learn C++".to_string(),
+        "Learn C#".to_string()
+    ];
+
+    let mut done_curr: usize = 0;
+
+    let mut ui = Ui::default();
 
     while !quit {
-        for (index, todo) in todos.iter().enumerate() {
-            let pair = {
-                if todo_curr == index {
-                    // Render in a different style
-                    HIGHLIGHT_PAIR
-                } else {
-                    REGULAR_PAIR
-                }
-            };
-            attron(COLOR_PAIR(pair));
-            mv(index as i32, 1);
-            addstr(*todo);
-            attroff (COLOR_PAIR(pair));
-        }  
+        ui.begin(0, 0);
+        {
+            ui.label("TODO: ", REGULAR_PAIR);
+            ui.begin_list(todo_curr);
+            for (index, todo) in todos.iter().enumerate() {
+                ui.list_element(&format!("- [ ] {}", todo), index);
+            }  
+            ui.end_list();
+            ui.label("-------------------------------------", REGULAR_PAIR);
+
+            ui.label("DONE: ", REGULAR_PAIR);
+            ui.begin_list(done_curr);
+            for(index, done) in dones.iter().enumerate() {
+                ui.list_element(&format!("- [x] {}", done), index);
+
+            }
+            ui.end_list();
+        }
+
+        ui.end();
         
         refresh();
+
         let key = getch();
 
         match key as u8 as char{
