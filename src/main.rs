aca1d4b02884bb955bc744f222e637e5568acb0c
Rajesh Pillai <pillai.rajesh@gmail.com>
2023-03-19 09:46:43 +0530

    HIGHLIGHT current todo item

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use ncurses::*;
 
+const REGULAR_PAIR: i16 = 0;
+const HIGHLIGHT_PAIR: i16 = 1;
+
 fn main() {
     initscr();
+
+    start_color();
+    init_pair(REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK)    ;
+    init_pair(HIGHLIGHT_PAIR, COLOR_BLACK, COLOR_WHITE);
     
     let mut quit = false;
 
@@ -11,10 +18,22 @@ fn main() {
         "Learn Kubernetes"
     ];
 
+    let todo_curr: usize = 0;
+
     while !quit {
-        for (row, todo) in todos.iter().enumerate() {
-            mv(row as i32, 1);
+        for (index, todo) in todos.iter().enumerate() {
+            let pair = {
+                if todo_curr == index {
+                    // Render in a different style
+                    HIGHLIGHT_PAIR
+                } else {
+                    REGULAR_PAIR
+                }
+            };
+            attron(COLOR_PAIR(pair));
+            mv(index as i32, 1);
             addstr(*todo);
+            attroff (COLOR_PAIR(pair));
         }  
         
         refresh();
