3cb197c466b16ebfcd33fbdaf72e7fefd6e8e396
Rajesh Pillai <pillai.rajesh@gmail.com>
2023-03-19 09:34:08 +0530

    Render a list of todos

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,24 @@ use ncurses::*;
 
 fn main() {
     initscr();
-    addstr("Hello, World");
-    refresh();
     
     let mut quit = false;
 
-    let mut todos = vec![
+    let todos = vec![
         "Learn Rust", 
         "Learn Zig",
         "Learn Kubernetes"
     ];
 
     while !quit {
+        for (row, todo) in todos.iter().enumerate() {
+            mv(row as i32, 1);
+            addstr(*todo);
+        }  
+        
+        refresh();
         let key = getch();
+
         match key as u8 as char{
             'q' => quit = true,
             _ => {}
